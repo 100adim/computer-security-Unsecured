8f1be23179dd28d02783f06bb8930e84c24cfe0a
shlomige <shlomi@geva4u.com>
2025-06-03 16:47:25 +0300

    add customer list

--- communication_ltd/settings.py
+++ communication_ltd/settings.py
@@ -67,3 +67,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_USER_EMAIL = 'hitltdproject@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'hitltdproject@gmail.com'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+LOGIN_URL = '/login/'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- mainapp/models.py
+++ mainapp/models.py
@@ -14,6 +14,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     id_number = models.CharField(max_length=20, unique=True)
+    created_by = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
